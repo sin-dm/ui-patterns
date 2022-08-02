b79c582e4f13ad4a8c4afd7bab1d36bb4dd0a2c7
sindm <sinitsyn.d.i@gmail.com>
2022-08-02 20:29:23 +0300

    Add code from lesson

---
+++ src/test/java/ru/yandex/uipattern/BaseTest.java
@@ -0,0 +1,30 @@
+package ru.yandex.uipattern;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+//    public WebDriver operaDriver() {
+//
+//    }
+
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
